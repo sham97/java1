ace705736b91a583679f70f951baa05918585799
Illthizam9721 <illthizamn@kainovation.com>
2023-08-04 00:38:49 +0530

    new file commit

---
+++ DisplayCurrentDateTime.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DisplayCurrentDateTime {
+    public static void main(String[] args) {
+        // Get the current date and time
+        Date currentDate = new Date();
+
+        // Display the current date and time
+        System.out.println("Current Date and Time: " + currentDate);
+    }
+}
